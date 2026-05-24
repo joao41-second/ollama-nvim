20771e98a466e1f7cdab2cc521523e19ec981678
joao41second <jfigueiredo095@gmail.com>
2026-05-24 10:25:33 +0100

    f

--- lua/ollama/init.lua
+++ lua/ollama/init.lua
@@ -1,31 +1,32 @@
 local M = {}
 
 function M.create_temp_file()
-    local temp_file = vim.fn.tempname() -- gera um nome temporário
-    local temp_path = temp_file .. ".tmp" -- ou qualquer extensão
+    local temp_file = vim.fn.tempname()
+    local temp_path = temp_file .. ".tmp"
 
-    -- Cria o arquivo temporário
     local file = io.open(temp_path, "w")
+
     if not file then
-        vim.notify("Erro ao criar arquivo temporário", vim.notify.ERROR)
+        vim.notify(
+            "Erro ao criar arquivo temporário",
+            vim.log.levels.ERROR
+        )
         return nil
     end
 
     file:write("Este é um arquivo temporário gerado por Lua.\n")
     file:close()
 
-    vim.notify("Arquivo temporário criado: " .. temp_path, vim.notify.INFO)
+    vim.notify(
+        "Arquivo temporário criado: " .. temp_path,
+        vim.log.levels.INFO
+    )
+
     return temp_path
 end
 
-  vim.api.nvim_create_user_command("createtempfile",function ()
-      require("ollama").create_temp_file();
-
-  end,{}
-  )
-
-
+vim.api.nvim_create_user_command("CreateTempFile", function()
+    require("ollama").create_temp_file()
+end, {})
 
 return M
-
-
