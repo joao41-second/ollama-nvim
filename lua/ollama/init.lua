81bda641ef0a9a9f1add08e4fa2028dd0bf95676
joao41second <jfigueiredo095@gmail.com>
2026-05-24 14:29:31 +0100

    frist chat ia

--- lua/ollama/init.lua
+++ lua/ollama/init.lua
@@ -1,10 +1,11 @@
 local M = {}
 
-function M.create_temp_file()
-    local temp_file = vim.fn.tempname()
-    local temp_path = temp_file .. ".tmp"
+M.temp_file = vim.fn.tempname()
+M.temp_path = temp_file .. ".tmp"
 
-    local file = io.open(temp_path, "w")
+function M.create_temp_file()
+
+    local file = io.open(M.temp_path, "w")
 
     if not file then
         vim.notify("Erro ao criar arquivo", vim.log.levels.ERROR)
@@ -19,9 +20,54 @@ function M.create_temp_file()
     vim.cmd("edit " .. temp_path)
 end
 
+function M.chat()
+    local prompt = vim.fn.input("Chat > ")
+
+    print("Você escreveu: " .. prompt)
+    return( prompt)
+end
+
+function M.curl_lms( mens)
+
+    local prompt = vim.fn.input("Chat > ")
+
+    local json = vim.fn.json_encode({
+        model = "qwen/qwen3-vl-4b",
+        messages = {
+            {
+                role = "user",
+                content = prompt
+            }
+        }
+    })
+    local result = vim.system({
+        "curl",
+        "-s",
+        "http://localhost:1234/v1/chat/completions",
+        "-H",
+        "Content-Type: application/json",
+        "-d",
+        json
+    }, { text = true }):wait()
+
+    local files = io.open(M.temp_path or "/tmp/nvim_lms.txt", "w")
+
+    print("Você escreveu: " ..  result.stdout)
+
+    files:write("new\n")
+    files:write(result.stdout or "", "\n")
+    files:close()
+
+    vim.notify(temp_path, vim.log.levels.INFO)
+
+    vim.cmd("edit " .. temp_path)
+end
 function M.setup()
     vim.api.nvim_create_user_command("CreateTempFile", function()
         M.create_temp_file()
+    end, {})
+    vim.api.nvim_create_user_command("Chats", function()
+         M.curl_lms()
     end, {})
 end
 
